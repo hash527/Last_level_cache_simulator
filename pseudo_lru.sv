0258e8140b0701d0fdf2c4e76a6c3b597b85afcb
karthik bandla <kbandla@pdx.edu>
2024-11-18 12:39:35 -0800

    pseudolru

--- pseudo_lru.sv
+++ pseudo_lru.sv
@@ -22,13 +22,13 @@ class BT;
         root.left = traverse_and_evict(root.left, array, count);
       end
     end
-    return root;
+    return root; 
   endfunction
 
   function automatic Node inorder_manipulate(int x[], ref int i, Node root);
     if (root != null) begin
       root.left = inorder_manipulate(x, i, root.left);
-      root.value = x[i];
+      root.value = x[i];  
       i += 1;
       root.right = inorder_manipulate(x, i, root.right);
     end
@@ -50,10 +50,57 @@ class BT;
   endfunction
 
   function void inorder(Node root);
-    if (root != null) begin
-      inorder(root.left);
-      $display(" %0d", root.value);
-      inorder(root.right);
-[No Name][+] [unix] (15:59 31/12/1969)                                                                                                                                                                     1,1 Top
--- INSERT --
+    if (root != null) begin  
+      inorder(root.left);         
+      $display(" %0d", root.value);  
+      inorder(root.right);  
+    end 
+  endfunction
+endclass
+
+module sample;
+  initial begin
+    int j = 0; 
+    int k = 0;
+    int count = 0;
+    int array[4];
+    int A[4] = '{0, 1, 0, 1};
+    int x[16] = '{1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1}; 
+    Node root;
+    BT bt = new();
+
+    root = new(0);
+    root.left = new(0);
+    root.right = new(0);
+    root.left.left = new(0);
+    root.left.right = new(0);
+    root.right.left = new(0);
+    root.right.right = new(0);
+    root.left.left.left = new(0);
+    root.left.left.right = new(0);
+    root.left.right.left = new(0);
+    root.left.right.right = new(0);
+    root.right.left.left = new(0);
+    root.right.left.right = new(0);
+    root.right.right.left = new(0);
+    root.right.right.right = new(0);
+
+    $display("Tree after appending zeros (Inorder Traversal):");
+    bt.inorder(root);
+
+    root = bt.inorder_manipulate(x, j, root);
+    $display("Tree after appending x array (Inorder Traversal):");
+    bt.inorder(root);
+
+    root = bt.manipulate(A, k, root);
+    $display("Tree after appending A array (Inorder Traversal):");
+    bt.inorder(root);
+
+    count = 0;
+    root = bt.traverse_and_evict(root, array, count);
+
+    $display("Way to be evicted:%p",array);
+    
+  end
+endmodule
 
